634ad8b0b966743bf71d4c64d3222957decfad69
seidlstr3 <131800333+seidlstr3@users.noreply.github.com>
2023-05-04 14:05:57 +0200

    Update 07_clustering.py

--- 07_clustering.py
+++ 07_clustering.py
@@ -6,6 +6,8 @@ matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
+import numpy as np
+
 
 from sklearn.cluster import KMeans
 from sklearn import datasets
@@ -24,8 +26,8 @@ variable_names = iris.feature_names
 #print(X[0:10,])
 #print(iris)
 
-
-clustering = KMeans(n_clusters=10, random_state=5)
+N=3
+clustering = KMeans(n_clusters=N, random_state=5)
 clustering.fit(X)
 
 
@@ -34,7 +36,29 @@ clustering.fit(X)
 x=iris_df.loc[:,'Petal_Length']
 y=iris_df.loc[:,'Petal_Width']
 
-plt.scatter(x=x,y=y)
+print(iris.target)
+
+
+colorz = np.random.randint(0,3,len(x))
+#iris_df[]
+#colorz=color_theme[iris.target]
+
+my_map = plt.get_cmap('viridis', 3)
+
+#########################################################
+
+plt.subplot(1,2,1)
+
+plt.scatter(x=x,y=y, c=iris.target, cmap=my_map)
+
+
+plt.subplot(1,2,2)
+
+#mapped_colors= np.vectorize(clustering.labels_.map({0:1, 1:0, 2:2})
+mapped_colors = np.choose(clustering.labels_, [1,0,2]).astype(np.int64)
+
+plt.scatter(x=x,y=y, c=mapped_colors, cmap= plt.get_cmap('viridis', N))
+
 
 #Two  lines to make our compiler able to draw:
 plt.savefig(sys.stdout.buffer)
